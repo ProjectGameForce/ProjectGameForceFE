e9eb440cb8874de0e164040d12b2ee919c3cf6db
Victor <victorgboynton@gmail.com>
2023-12-13 23:46:07 -0700

    Delete these changes if needed

--- app/ReviewProps.tsx
+++ app/ReviewProps.tsx
@@ -10,7 +10,7 @@ export const Review = ({ reviewObj, styles }: ReviewProps) => {
   const { name, review } = reviewObj;
   return (
     <div
-      className={`-mx-10  w-64 h-80 border-2 rounded-xl ${styles} border-gray-600 bg-gray-200`}
+      className={`-mx-5 hover:w-72 hover:mx-0 hover:h-96 hover:text-xl w-64 h-80 border-2 rounded-xl ${styles} border-gray-600 bg-gray-200`}
     >
       <div className="m-5">
         <q>{review}</q>
