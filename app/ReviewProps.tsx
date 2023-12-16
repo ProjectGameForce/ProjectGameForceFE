b37c8da84fc0aef399f5252f15e3b7ab26c0174d
victorgboynton <victorgboynton@gmail.com>
2023-12-16 12:02:16 -0700

    Functionality: I'm trying man

--- app/ReviewProps.tsx
+++ app/ReviewProps.tsx
@@ -3,17 +3,15 @@ import React from "react";
 import { ReviewObj } from "./page";
 
 type ReviewProps = {
-  reviewObj: ReviewObj;
-  styles: string;
+	reviewObj: ReviewObj;
+	styles: string;
 };
 export const Review = ({ reviewObj, styles }: ReviewProps) => {
-  const { name, review } = reviewObj;
-  return (
-    <div
-      className={`hover:w-72 hover:mx-0 hover:h-96 hover:text-xl w-[100%] h-60 mx-10 rounded-xl ${styles} `}
-    >
-      <q>{review}</q>
-      <h1 className="float-right mr-10 font-bold">-{name}</h1>
-    </div>
-  );
+	const { name, review } = reviewObj;
+	return (
+		<div className={`hover:text-xl w-full  mx-10 ${styles} `}>
+			<q>{review}</q>
+			<h1 className="float-right mr-10 font-bold">-{name}</h1>
+		</div>
+	);
 };
--- app/Spinner.tsx
+++ app/Spinner.tsx
@@ -5,53 +5,49 @@ import { Review } from "./ReviewProps";
 import { SliderButton } from "./SliderButton";
 
 export function Spinner() {
-  const [currentIndex, setCurrentIndex] = useState(0);
-  /* const goToPrev = () => {
+	const [currentIndex, setCurrentIndex] = useState(0);
+	/* const goToPrev = () => {
     setCurrentIndex((currentCurrentIndex) => {
       return currentCurrentIndex === 0
         ? reviewList.length - 1
         : currentCurrentIndex - 1;
     });
   }; */
-  const goNext = () => {
-    setCurrentIndex((currentCurrentIndex) => {
-      return currentCurrentIndex === reviewList.length - 1
-        ? 0
-        : currentCurrentIndex + 1;
-    });
-  };
+	const goNext = () => {
+		setCurrentIndex((currentIndex) => (currentIndex + 1) % reviewList.length);
+	};
 
-  useEffect(() => {
-    let interval: any;
-    const startAutoScroll = () => {
-      interval = setInterval(goNext, 1000);
-    };
-    startAutoScroll();
-    /* const stopScroll = () => {
+	useEffect(() => {
+		let interval: any;
+		const startAutoScroll = () => {
+			interval = setInterval(goNext, 1000);
+		};
+		startAutoScroll();
+		/* const stopScroll = () => {
       clearInterval(interval);
     }; */
 
-    /* const spinnerElement = document.getElementById("spinnerId");
+		/* const spinnerElement = document.getElementById("spinnerId");
     spinnerElement?.addEventListener("mouseenter", stopScroll);
     spinnerElement?.addEventListener("mouseleave", startAutoScroll); */
-    return () => {
-      clearInterval(interval);
-      /* spinnerElement?.removeEventListener("mouseenter", stopScroll);
+		return () => {
+			clearInterval(interval);
+			/* spinnerElement?.removeEventListener("mouseenter", stopScroll);
       spinnerElement?.removeEventListener("mouseleave", startAutoScroll); */
-    };
-  }, [reviewList, goNext]);
-  return (
-    <div className="mx-10 justify-center items-center my-20 w-[70%] ">
-      <div
-        className=" flex flex-row justify-center h-72 transition-transform duration-[10000ms] ease-in-out"
-        style={{ transform: `translateX(-${currentIndex * 100}%)` }}
-      >
-        {reviewList.map((item, index) => (
-          <div className="w-[100%]">
-            <Review reviewObj={item} key={index} styles={""} />
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+		};
+	});
+	return (
+		<div className="mx-10 justify-center items-center my-20 w-[70%] overflow-hidden ">
+			<div
+				className="transition-transform duration-[10000ms] ease-in-out"
+				style={{ transform: `translateX(-${currentIndex * 100}%)` }}
+			>
+				{reviewList.map((item, index) => (
+					<div className="" key={index}>
+						<Review reviewObj={item} styles={""} />
+					</div>
+				))}
+			</div>
+		</div>
+	);
 }
