cda8c1c236f046c149a39098eb24273caefc7da0
victorgboynton <victorgboynton@gmail.com>
2023-12-17 18:42:47 -0700

    I may have messed up

--- app/Spinner.tsx
+++ app/Spinner.tsx
@@ -5,65 +5,41 @@ import { Review } from "./ReviewProps";
 import { SliderButton } from "./SliderButton";
 
 export function Spinner() {
-  const [currentIndex, setCurrentIndex] = useState(0);
-  /* const goToPrev = () => {
-    setCurrentIndex((currentCurrentIndex) => {
-      return currentCurrentIndex === 0
-        ? reviewList.length - 1
-        : currentCurrentIndex - 1;
-    });
-  }; */
-  const goNext = () => {
-    setCurrentIndex((currentCurrentIndex) => {
-      return currentCurrentIndex === reviewList.length - 1
-        ? 0
-        : currentCurrentIndex + 1;
-    });
-  };
+	const [currentIndex, setCurrentIndex] = useState(0);
 
-  useEffect(() => {
-    let interval: any;
-    const startAutoScroll = () => {
-      interval = setInterval(goNext, 10000);
-    };
-    startAutoScroll();
-    /* const stopScroll = () => {
-      clearInterval(interval);
-    }; */
+	const goNext = () => {
+		setCurrentIndex((currentIndex) => (currentIndex + 1) % reviewList.length);
+	};
 
-    /* const spinnerElement = document.getElementById("spinnerId");
-    spinnerElement?.addEventListener("mouseenter", stopScroll);
-    spinnerElement?.addEventListener("mouseleave", startAutoScroll); */
-    return () => {
-      clearInterval(interval);
-      /* spinnerElement?.removeEventListener("mouseenter", stopScroll);
-      spinnerElement?.removeEventListener("mouseleave", startAutoScroll); */
-    };
-  }, [reviewList, goNext]);
-  return (
-    <div className="flex flex-row mx-10 justify-center items-center my-20 w-[70%] overflow-hidden ">
-      <div
-        className="flex flex-col md:flex-row justify-center h-72 transition-transform duration-[10000ms]"
-        //style={{ transform: `translateX(-${300}%)` }}
-      >
-        <Review
-          reviewObj={
-            reviewList[
-              currentIndex === 0 ? reviewList.length - 1 : currentIndex - 1
-            ]
-          }
-          styles={""}
-        />
-        <Review reviewObj={reviewList[currentIndex]} styles={""} />
-        <Review
-          reviewObj={
-            reviewList[
-              currentIndex === reviewList.length - 1 ? 0 : currentIndex + 1
-            ]
-          }
-          styles={""}
-        />
-      </div>
-    </div>
-  );
+	useEffect(() => {
+		const interval = setInterval(goNext, 10000);
+
+		/* const stopScroll = () => {
+        clearInterval(interval);
+      }; */
+
+		/* const spinnerElement = document.getElementById("spinnerId");
+      spinnerElement?.addEventListener("mouseenter", stopScroll);
+      spinnerElement?.addEventListener("mouseleave", startAutoScroll); */
+		return () => {
+			clearInterval(interval);
+			/* spinnerElement?.removeEventListener("mouseenter", stopScroll);
+        spinnerElement?.removeEventListener("mouseleave", startAutoScroll); */
+		};
+	}, []);
+	return (
+		<div className="mx-10 justify-center items-center my-20 w-[70%] overflow-hidden ">
+			<div className="flex flex-row">
+				{reviewList.map((item, index) => (
+					<div
+						className="min-w-full max-w-full transition-transform duration-[10000ms] ease-in-out"
+						key={index}
+						style={{ transform: `translateX(-${currentIndex * 100}%)` }}
+					>
+						<Review reviewObj={item} styles={""} />
+					</div>
+				))}
+			</div>
+		</div>
+	);
 }
