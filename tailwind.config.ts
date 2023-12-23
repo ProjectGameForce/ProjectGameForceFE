22c4e812eeec61501756c4056f9663afe9157542
victorgboynton <victorgboynton@gmail.com>
2023-12-23 02:52:59 -0700

    Messing around

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,29 +1,30 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 const config: Config = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-      fontFamily: {
-          pixel: ["'Pixelify Sans'"],
-          monoRoboto: ["'Roboto Mono'"],
-      },
-      animation: {
-        rotate: 'rotate 20s linear infinite alternate'
-      },
-      boxShadow:{
-        edgeBlur: 'inset 20px 20px 20px 20px rgba(0, 0, 0, 0.9);'
-      }
-    },
-  },
-  plugins: [],
-}
-export default config
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			backgroundImage: {
+				"gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+				"gradient-conic":
+					"conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+			},
+			fontFamily: {
+				pixel: ["'Pixelify Sans'"],
+				monoRoboto: ["'Roboto Mono'"],
+			},
+			animation: {
+				rotate: "rotate 20s linear infinite alternate",
+				"fade-in-out": "fade-in-out linear",
+			},
+			boxShadow: {
+				edgeBlur: "inset 20px 20px 20px 20px rgba(0, 0, 0, 0.9);",
+			},
+		},
+	},
+	plugins: [],
+};
+export default config;
